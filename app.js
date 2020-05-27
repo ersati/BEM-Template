e0259a6fde057a0f43f2205d892d854430e2eb5a
Piotr Pacewicz <piotrp92@gmail.com>
2020-05-27 20:20:43 +0100

    add another article and background

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const button = document.querySelector(".article__button");
 const article = document.querySelector(".article__invisible");
+const buttons = document.querySelectorAll(".article__button");
+const articles = document.querySelectorAll(".article__invisible");
 
-const addClass = () => {
-  article.classList.toggle("article__invisible--active");
-  button.classList.toggle("article__button--invisible");
+// const addClass = () => {
+//   article.classList.toggle("article__invisible--active");
+
+//   if (article.classList.contains("article__invisible--active")) {
+//     button.textContent = "Hide";
+//   } else {
+//     button.textContent = "Read More";
+//   }
+// };
+// button.addEventListener("click", addClass);
+
+const addMultipleClasses = () => {
+  articles.forEach((article) => {
+    article.classList.toggle("article__invisible--active");
+    if (article.classList.contains("article__invisible--active")) {
+      button.textContent = "Hide";
+    } else {
+      button.textContent = "Read More";
+    }
+  });
 };
-button.addEventListener("click", addClass);
+buttons.forEach((button) => {
+  button.addEventListener("click", addMultipleClasses);
+});
