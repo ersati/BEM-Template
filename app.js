28a591db39fba80887e1946892b99b4e63640385
Piotr Pacewicz <piotrp92@gmail.com>
2020-06-01 20:53:35 +0100

    add king mieszko I to html

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const button = document.querySelector(".article__button");
 const article = document.querySelector(".article__invisible");
 const buttons = document.querySelectorAll(".article__button");
 const articles = document.querySelectorAll(".article__invisible");
-
+console.log(articles);
 // const addClass = () => {
 //   article.classList.toggle("article__invisible--active");
 
