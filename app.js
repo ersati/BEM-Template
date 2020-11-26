d11a0fdf4f6ea0c7cf6ce554b963dd7b8623562e
Peter <piotrp92@gmail.com>
2020-11-26 20:34:58 +0000

    use beautify

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const buttons = document.querySelectorAll(".article__button");
 const articles = document.querySelectorAll(".article__invisible");
 
-const changeButton = ({target}) => {
-  const activeArticle = target.previousElementSibling
-  const activeButton = target
-   console.log(target.previousElementSibling)
-activeArticle.classList.toggle("article__invisible--active");
-if (activeArticle.classList.contains("article__invisible--active")) {
+const changeButton = ({
+    target
+}) => {
+    const activeArticle = target.previousElementSibling
+    const activeButton = target
+    console.log(target.previousElementSibling)
+    activeArticle.classList.toggle("article__invisible--active");
+    if (activeArticle.classList.contains("article__invisible--active")) {
         activeButton.textContent = "Hide";
-      } else { 
+    } else {
         activeButton.textContent = "Read More";
-      }
+    }
 }
 
 buttons.forEach(button => {
-  button.addEventListener('click', changeButton)
+    button.addEventListener('click', changeButton)
 })
