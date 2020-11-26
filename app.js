bd88bbfb7e0631d3d1dbef9fcc64c9dd6c217147
Peter <piotrp92@gmail.com>
2020-11-26 04:55:11 +0000

    add functionality to button

--- app.js
+++ app.js
@@ -1,29 +1,18 @@
-const button = document.querySelector(".article__button");
-const article = document.querySelector(".article__invisible");
 const buttons = document.querySelectorAll(".article__button");
 const articles = document.querySelectorAll(".article__invisible");
-console.log(articles);
-// const addClass = () => {
-//   article.classList.toggle("article__invisible--active");
 
-//   if (article.classList.contains("article__invisible--active")) {
-//     button.textContent = "Hide";
-//   } else {
-//     button.textContent = "Read More";
-//   }
-// };
-// button.addEventListener("click", addClass);
+const changeButton = ({target}) => {
+  const activeArticle = target.previousElementSibling
+  const activeButton = target
+   console.log(target.previousElementSibling)
+activeArticle.classList.toggle("article__invisible--active");
+if (activeArticle.classList.contains("article__invisible--active")) {
+        activeButton.textContent = "Hide";
+      } else { 
+        activeButton.textContent = "Read More";
+      }
+}
 
-const addMultipleClasses = () => {
-  articles.forEach((article) => {
-    article.classList.toggle("article__invisible--active");
-    if (article.classList.contains("article__invisible--active")) {
-      button.textContent = "Hide";
-    } else {
-      button.textContent = "Read More";
-    }
-  });
-};
-buttons.forEach((button) => {
-  button.addEventListener("click", addMultipleClasses);
-});
+buttons.forEach(button => {
+  button.addEventListener('click', changeButton)
+})
